69eb8d95c2c06e6ededebff6b89e9ef0b64bc2b4
David França <david.franca10@aluno.ifce.edu.br>
2025-01-25 21:28:26 -0300

    Add files via upload

---
+++ UrnaEletrônica/src/br/edu/urnaEletronica/Eleitor.java
@@ -0,0 +1,21 @@
+package br.edu.urnaEletronica;
+
+public class Eleitor {
+	private String nome;
+	private String tituloEleitoral;
+	private boolean votou;
+	
+	public Eleitor(String nome, String titulo) {
+		this.nome = nome;
+		this.tituloEleitoral = titulo;
+		this.votou = false;
+	}
+	
+	public boolean podeVotar() {
+		return !votou;
+	}
+	
+	public String gettituloEleitoral() {
+		return tituloEleitoral;
+	}
+}
